b3025b4b950df4e0d18a134ca14fdcbbdf2cf4d5
mdeyo <mdeyo@mit.edu>
2014-02-24 00:51:42 -0500

    connected gettingStarted activity and added menyou logo into gS activity

--- gen/edu/mit/menyou/R.java
+++ gen/edu/mit/menyou/R.java
@@ -22,13 +22,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menyou_logo=0x7f020001;
+        public static final int logo5=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button2=0x7f080003;
-        public static final int button_getStart=0x7f080002;
-        public static final int sub_title1=0x7f080001;
-        public static final int title1=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int button2=0x7f080006;
+        public static final int button_getStart=0x7f080005;
+        public static final int gs_logo=0x7f080000;
+        public static final int gs_textView1=0x7f080001;
+        public static final int sub_title1=0x7f080004;
+        public static final int textView1=0x7f080002;
+        public static final int title1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_getting_started=0x7f030000;
@@ -43,6 +48,8 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int button_text=0x7f050004;
         public static final int button_text_2=0x7f050005;
+        public static final int getting_started_advice=0x7f050008;
+        public static final int getting_started_intro=0x7f050007;
         public static final int sub_title1=0x7f050003;
         public static final int title1=0x7f050002;
         public static final int title_activity_getting_started=0x7f050006;
--- src/edu/mit/menyou/Home.java
+++ src/edu/mit/menyou/Home.java
@@ -1,6 +1,7 @@
 package edu.mit.menyou;
 
 import java.io.BufferedReader;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -14,11 +15,13 @@ import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
@@ -32,7 +35,6 @@ public class Home extends Activity {
 		setContentView(R.layout.activity_home);
 		Parse.initialize(this, "4EPEC8gdyy1UVP4yC0pRpfM30zpgGMGkoMdeu9p7", "1DxRG10TudyhJwAR4jildKVne8q3PjqNHVvpzIlY");
 		
-		final String title = getResources().getString(R.string.title1);
         final TextView tv1 = (TextView) findViewById(R.id.title1);
         final TextView tv2 = (TextView) findViewById(R.id.sub_title1);
         final Button button = (Button) findViewById(R.id.button_getStart);
@@ -44,6 +46,23 @@ public class Home extends Activity {
         tv2.setTypeface(type);
         button.setTypeface(type);
         button2.setTypeface(type);
+        
+        //Listening to button event
+        button.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View arg0) {
+                //Starting a new Intent
+                Intent nextScreen = new Intent(getApplicationContext(), GettingStarted.class);
+                /*
+                //Sending data to another Activity
+                nextScreen.putExtra("name", inputName.getText().toString());
+                nextScreen.putExtra("email", inputEmail.getText().toString());
+ 
+                Log.e("n", inputName.getText()+"."+ inputEmail.getText());
+ 				*/
+                startActivity(nextScreen);
+                }
+        });
      
         
         //////////////////////////////////////////////////////////////////////
