330c4164a340499d21e86bb91e01740ac148dfb9
mdeyo <mdeyo@mit.edu>
2014-02-24 00:13:13 -0500

    made new GetStarted activity and changed text/buttons on Main

--- gen/edu/mit/menyou/R.java
+++ gen/edu/mit/menyou/R.java
@@ -24,24 +24,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int hello_text=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int button2=0x7f080003;
+        public static final int button_getStart=0x7f080002;
+        public static final int sub_title1=0x7f080001;
+        public static final int title1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_home=0x7f030000;
+        public static final int activity_getting_started=0x7f030000;
+        public static final int activity_home=0x7f030001;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int getting_started=0x7f070000;
+        public static final int home=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int button_text=0x7f050004;
         public static final int button_text_2=0x7f050005;
-        public static final int goodbye_world=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int sub_title1=0x7f050003;
+        public static final int title1=0x7f050002;
+        public static final int title_activity_getting_started=0x7f050006;
     }
     public static final class style {
         /** 
--- src/edu/mit/menyou/Home.java
+++ src/edu/mit/menyou/Home.java
@@ -32,39 +32,19 @@ public class Home extends Activity {
 		setContentView(R.layout.activity_home);
 		Parse.initialize(this, "4EPEC8gdyy1UVP4yC0pRpfM30zpgGMGkoMdeu9p7", "1DxRG10TudyhJwAR4jildKVne8q3PjqNHVvpzIlY");
 		
-		final String hello = getResources().getString(R.string.hello_world);
-        final String goodbye = getResources().getString(R.string.goodbye_world);
-        final TextView tv = (TextView) findViewById(R.id.hello_text);
-        final Button button = (Button) findViewById(R.id.button);
+		final String title = getResources().getString(R.string.title1);
+        final TextView tv1 = (TextView) findViewById(R.id.title1);
+        final TextView tv2 = (TextView) findViewById(R.id.sub_title1);
+        final Button button = (Button) findViewById(R.id.button_getStart);
         final Button button2 = (Button) findViewById(R.id.button2);
         
         // set up the font
         Typeface type = Typeface.createFromAsset(getAssets(),"fonts/bubblebo.ttf");
-        tv.setTypeface(type);
+        tv1.setTypeface(type);
+        tv2.setTypeface(type);
         button.setTypeface(type);
         button2.setTypeface(type);
-        
-        button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				if(tv.getText().equals(hello)) {
-					tv.setText(goodbye);
-				} else {
-					tv.setText(hello);
-				}
-			}
-		});
-        
-        button2.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				if(tv.getText().equals(hello)) {
-					tv.setText(goodbye);
-				} else {
-					tv.setText(hello);
-				}
-			}
-		});
+     
         
         //////////////////////////////////////////////////////////////////////
         // test that menyou is connected to Parse
