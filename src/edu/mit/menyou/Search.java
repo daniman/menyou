ba548906da31e7d123ae07549229b81962896783
Danielle Man <daniman0808@gmail.com>
2014-03-20 12:56:28 -0400

    added json to search view

--- src/edu/mit/menyou/Search.java
+++ src/edu/mit/menyou/Search.java
@@ -1,10 +1,21 @@
 package edu.mit.menyou;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -50,11 +61,18 @@ public class Search extends Activity {
 		final AutoCompleteTextView search_input1 = (AutoCompleteTextView) findViewById(R.id.search_input);
 		search_input1.setAdapter(adapter1);
 	    
+		
+		String location = "42.3598,-71.0921";
+        String radius = "200";
+        LocuTask task = new LocuTask();
+        task.execute(location, radius);		
+		
 		//Listening to button event
         searchButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
             	search_history.add(search_input.getText().toString());
             	Toast.makeText(Search.this, "searching", Toast.LENGTH_SHORT).show();
+            	adapter.notifyDataSetChanged();
             	adapter1.notifyDataSetChanged();
             }
         });
@@ -91,4 +109,67 @@ public class Search extends Activity {
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
+	
+	
+	
+	class LocuTask extends AsyncTask<String, Integer, JSONObject> {
+		
+		private static final String BASE_URL = "http://api.locu.com/v1_0/venue/search/?api_key=1a75b559bec4e3c7b00f0cc06d17356705599303";
+		private String request_url;
+		private JSONObject jsonData;
+		
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			JSONObject json = null;
+			request_url = BASE_URL + "&location=" + params[0] + "&radius=" + params[1];
+			try {
+			URL url = new URL(request_url);
+	          System.out.println(url.toString());
+	          BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream())); // read the server's output
+	          String serverOutput;
+	          String output = "";
+	          while ((serverOutput = in.readLine()) != null) {
+	              output = serverOutput;
+	          }
+	          json = new JSONObject(output);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return json;
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject result) {
+			jsonData = result;
+			JSONArray restaurants;
+			try {
+				restaurants = jsonData.getJSONArray("objects");
+				for (int i=0; i<restaurants.length() ;i++) {
+					JSONObject rest = restaurants.getJSONObject(i);
+					System.out.println(rest.get("name"));
+					search_history.add(rest.getString("name"));
+					
+					
+//					search_history.add(search_input.getText().toString());
+//	            	Toast.makeText(Search.this, "searching", Toast.LENGTH_SHORT).show();
+//	            	adapter.notifyDataSetChanged();
+//	            	adapter1.notifyDataSetChanged();
+					
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
 }
